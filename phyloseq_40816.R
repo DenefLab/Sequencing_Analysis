22953828dd17673fe2c9971093c521a48d2c5e5b
Dylan Baker <bakerdyl@umich.edu>
2022-01-07 14:44:52 -0500

    Added .R to end of R files to allow them to be opened in RStudio. Added .Rpoj file to allow working directory to be set automatically to this folder. Added mothur files that were missing.

---
+++ phyloseq_40816.R
@@ -0,0 +1,251 @@
+#atttempting to work with sequencing data in order to do analysis for UROP poster
+#reading off of Michelle Berry's Phyloseq Tutorial
+#install necessary packages
+install.packages("vegan")
+source("http://bioconductor.org/biocLite.R")
+biocLite("phyloseq")
+
+#load necessary packages 
+library(ggplot2)
+library(magrittr)
+library(dplyr)
+library(scales)
+library(grid)
+library(reshape2)
+library(phyloseq)
+library(vegan)
+
+#set-up working directory
+setwd("C:/Users/morga/Documents/UROP/Denef/Morgan/")
+
+#import data
+sharedfile = "Sed_QM.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared"
+taxfile = "Sed_QM.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.an.unique_list.0.03.cons.taxonomy"
+mapfile = "Metadata_Table_Read.csv"
+
+#import mothur data and metadata
+mothur_data <- import_mothur(mothur_shared_file = sharedfile, mothur_constaxonomy_file = taxfile)
+metadata <- read.csv(mapfile)
+#convert metadata to phyloseq format
+metadata <- sample_data(metadata)
+rownames(metadata) <- metadata$Original_Name
+
+#merge metadata into mothur file
+moth.merge = merge_phyloseq(mothur_data, metadata)
+
+#reformat taxonomy file column names 
+colnames(tax_table(moth.merge))
+colnames(tax_table(moth.merge)) <- c("Kingdom", "Phylum", "Class", "Order", 
+ "Family", "Genus", "Rank7", "Rank8")
+
+#filter out samples that I do not want to use, on the tutorial it says to use sample type
+#but for my metadata table I made a column named "Use" that has the same affect
+moth.sub <- subset_samples(moth.merge, Use == "Sample")
+moth.sub <- prune_taxa(taxa_sums(moth.sub) > 0, moth.sub) #unsure of what this step does
+##############################################################################################
+
+##############################################################################################
+#make new data.frame by removing any eukaryotic cells from the (tax_table(moth.sub))
+Final <-
+  moth.sub %>%
+  subset_taxa(Kingdom == "Bacteria" &
+                Family != "mitochondria" &
+                Class != "Chloroplast")
+#Scale the reads for "Final" by using code originally from Michelle Berry 
+#############################################################################
+#Begin graphing with set that has removed eukaryotic (should not be different)
+theme_set(theme_bw())
+ggplot(data.frame(sum = sample_sums(Final)), aes(sum)) + 
+  geom_histogram(color = "black", fill = "indianred", stat = "bin", binwidth = 500) +
+  ggtitle("Distribution of sample sequencing depth") + 
+  xlab("Read counts") + 
+  ylab("")
+
+#Graph with eukaryotic cells included (we don't expect this to be different)
+ggplot(data.frame(sum = sample_sums(moth.sub)), aes(sum)) +
+  geom_histogram(color = "black", fill = "blue") +
+  ggtitle("Distribution of Sample Sequencing Depth with Chloroplasts") +
+  ylab("") + xlab("Read Counts")
+#graphs did differ, meaning that there were some cholorplast/eukaryotic reads involved
+#########################################################################################################################
+
+#########################################################################################################################
+#stacked bar plot to determine changes in community composition of Transect Sediment samples
+sed_phylum <- Final %>%
+  subset_samples(Type == "Sediment" | Cruise == "Transect") %>%
+  tax_glom(taxrank = "Phylum") %>%                  
+  transform_sample_counts(function(x) {x/sum(x)} ) %>% 
+  psmelt() %>%                                        
+  filter(Abundance > 0.05) %>%                        
+  arrange(Phylum)
+
+#set plylum colors
+phylum.colors <- c(
+  "#CBD588", "#5F7FC7", "orange","#DA5724", "#508578", "#CD9BCD",
+  "#AD6F3B", "#673770","#D14285", "#652926", "#C84248", 
+  "#8569D5", "#5E738F","#D1A33D", "#8A7C64", "#599861")
+
+ggplot(sed_phylum, aes(x = Month_Collected, y = Abundance, fill = Phylum)) + 
+ # facet_grid(Station~.) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = phylum.colors) +
+  #scale_x_discrete(
+    #breaks = c("7/8", "8/4", "9/2", "10/6"),
+    #labels = c("Jul", "Aug", "Sep", "Oct"), 
+    #drop = FALSE
+#  ) +
+  # Remove x axis title
+  theme(axis.title.x = element_blank()) + 
+  #
+  guides(fill = guide_legend(reverse = TRUE, keywidth = 1, keyheight = 1)) +
+  ylab("Relative Abundance (Phyla > 2%) \n") +
+  ggtitle("Phylum Composition of Lake Erie \n Bacterial Communities by Sampling Site") 
+#########################################################################################################
+
+#########################################################################################################  
+#Making a plot based on increasing read number to determine minimum to help normalize sample reads ()
+sums_FWDB <- data.frame(colSums(otu_table(Final)))
+colnames(sums_FWDB) <- "Sample_TotalSeqs"
+sums_FWDB$sample <- row.names(sums_FWDB)
+sums_FWDB <- arrange(sums_FWDB, Sample_TotalSeqs)
+
+ggplot(sums_FWDB, aes(x=reorder(sample, Sample_TotalSeqs), y = Sample_TotalSeqs)) + 
+  ylab("Number of Sequences per Sample") +
+  geom_bar(stat = "identity", colour="black",fill="cornflowerblue")  + xlab("Sample Name") + 
+  ggtitle("Total Number of Sequences per Sample") + 
+  theme(axis.text.x = element_text(colour = "black", size=6, angle=45, hjust = 1, vjust = 1))
+#################################################################################################################
+
+#################################################################################################################
+#Plotting Ordinations
+#scale reads to even depth/// This hhas already been done in the Formula section
+Full.scale.format <-
+  Final %>%
+  subset_samples(Use == "Sample") %>%
+  scale_reads(round = "round")
+
+#PCoA of all useable samples
+Full.pcoa <- ordinate(physeq = scale_Final, 
+                      method = "PCoA", 
+                      distance = "bray")
+
+#making plot
+plot_ordination(
+  physeq = scale_Final,
+  ordination = Full.pcoa,
+  color = "Type",
+  shape = "Cruise",
+  title = "PCoA of Lake Michigan and Muskegon Lake Samples"
+) + 
+  scale_color_manual(values = c("#a65628", "red", "#ffae19",
+                                "#4daf4a", "#1919ff", "darkorchid3", "magenta")
+  ) +
+  geom_point(aes(color = Type), alpha = 0.7, size = 4) +
+  geom_point(colour = "grey90", size = 1.5) 
+
+#########################################################################
+#PCoA of Sediment only
+SedimentPCoA <-
+  Final %>%
+  subset_samples(Type == "Sediment") %>%
+  scale_reads(round = "round")
+
+Full_Sed_PCoA <- ordinate(physeq = SedimentPCoA, 
+                      method = "PCoA", 
+                      distance = "bray")
+
+#making plot
+plot_ordination(
+  physeq = SedimentPCoA,
+  ordination = Full_Sed_PCoA,
+  color = "Lake",
+  shape = "Cruise",
+  title = "PCoA of Lake Michigan and Muskegon Lake Sediment Samples"
+) + 
+  scale_color_manual(values = c("darkorchid3", "red", "#ffae19",
+                                "#4daf4a", "#1919ff", "darkorchid3", "magenta")
+  ) +
+  geom_point(aes(color = Lake), alpha = 0.7, size = 4) +
+  geom_point(colour = "grey90", size = 1.5)
+######################################################################
+#PCoA for Mussel Samples only
+MusselPCoA <-
+  scale_Final %>%
+  subset_samples(Sed_or_Mus == "Mussel") %>%
+  scale_reads(round = "round")
+
+Full_Mus_PCoA <- ordinate(physeq = MusselPCoA, 
+                          method = "PCoA", 
+                          distance = "bray")
+
+#making plot
+plot_ordination(
+  physeq = MusselPCoA,
+  ordination = Full_Mus_PCoA,
+  color = "Type",
+ # shape = "Mussel.Classification",
+  title = "PCoA of Lake Michigan Mussel Samples"
+ ) + 
+  scale_color_manual(values = c("darkorchid3", "red", "#ffae19",
+                                "#4daf4a", "#1919ff", "darkorchid3", "magenta")
+  ) +
+  geom_point(aes(color = Type), alpha = 0.7, size = 4) +
+  geom_point(colour = "grey90", size = 1.5)
+################################################################################################################
+
+###############################################################################################################
+#Alpda Diversity
+#calculate min of sample reads
+min.lib <- min(sample_sums(Final))
+#it comes out at 7 and that number is bad so we will use 1,500
+
+# Initialize matrices to store richness and evenness estimates
+nsamp = nsamples(Final)
+trials = 100
+
+richness <- matrix(nrow = nsamp, ncol = trials)
+row.names(richness) <- sample_names(Final)
+
+evenness <- matrix(nrow = nsamp, ncol = trials)
+row.names(evenness) <- sample_names(Final)
+
+# It is always important to set a seed when you subsample so your result is replicable 
+set.seed(3)
+
+for (i in 1:100) {
+  # Subsample
+  r <- rarefy_even_depth(Final, sample.size = 1500, verbose = FALSE, replace = TRUE)
+  
+  # Calculate richness
+  rich <- as.numeric(as.matrix(estimate_richness(r, measures = "Observed")))
+  richness[ ,i] <- rich
+  
+  # Calculate evenness
+  even <- as.numeric(as.matrix(estimate_richness(r, measures = "InvSimpson")))
+evenness[ ,i] <- even
+}
+
+########FUNCTIONS#################################################################################################################
+matround <- function(x){trunc(x+0.5)}
+scale_Final <-scale_reads(physeq = Final, n = 5000, round = "matround")
+
+scale_reads <- function(physeq, n = min(sample_sums(physeq)), round = "floor") {
+
+# transform counts to n
+physeq.scale <- transform_sample_counts(physeq, 
+function(x) {(n * x/sum(x))}
+)
+
+# Pick the rounding functions
+if (round == "floor"){
+otu_table(physeq.scale) <- floor(otu_table(physeq.scale))
+} else if (round == "round"){
+otu_table(physeq.scale) <- round(otu_table(physeq.scale))
+}else if (round == "matround"){
+otu_table(physeq.scale) <- matround(otu_table(physeq.scale))
+}
+
+ # Prune taxa and return new phyloseq object
+ physeq.scale <- prune_taxa(taxa_sums(physeq.scale) > 0, physeq.scale)
+ return(physeq.scale)
+ }
